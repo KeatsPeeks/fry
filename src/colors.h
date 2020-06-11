1dafaa77087bfa4f00d8a25714d76c20dd1bbb39
Samuel Lacroix <KeatsPeeks@users.noreply.github.com>
2020-06-12 00:45:47 +0200

    Lowered MacOS minimal version, new colors

--- src/colors.h
+++ src/colors.h
@@ -2,9 +2,9 @@
 
 namespace app {
 
-    constexpr const static SDL_Color GRID_COLOR{0xB0, 0xB0, 0xB0, SDL_ALPHA_OPAQUE};
+    constexpr const static SDL_Color GRID_COLOR{0xF0, 0xF0, 0xF0, SDL_ALPHA_OPAQUE};
     constexpr const static SDL_Color TRANSPARENT_COLOR{0, 0, 0, SDL_ALPHA_TRANSPARENT};
-    constexpr const static SDL_Color DEAD_COLOR{0x80, 0x80, 0x80, SDL_ALPHA_OPAQUE};
-    constexpr const static SDL_Color ALIVE_COLOR{0x0A, 0x6E, 0xDC, SDL_ALPHA_OPAQUE};
+    constexpr const static SDL_Color DEAD_COLOR{0xB8, 0xD0, 0xF8, SDL_ALPHA_OPAQUE};
+    constexpr const static SDL_Color ALIVE_COLOR{0x34, 0x77, 0xEB, SDL_ALPHA_OPAQUE};
 
 }
